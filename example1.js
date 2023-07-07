fcf84b2fcd14ed76b332b714fc241c1d80b7e955
Nano191225 <93137582+Nano191225@users.noreply.github.com>
2023-07-07 18:40:51 +0900

    a

--- example1.js
+++ example1.js
@@ -7,6 +7,6 @@ fetch("https://api.github.com/repos/Unknown-Creators-Team/GBan/contents/list.jso
 .then(json => {
     const list = atob(json.content);
     GBan = JSON.parse(list);
-    console.log(JSON.stringify(GBan, null, 2));
+    console.debug(JSON.stringify(GBan, null, 2));
 })
 .catch(console.error);
--- example2.js
+++ example2.js
@@ -6,6 +6,6 @@ fetch("https://gban.un-known.xyz/list.json")
 .then(response => response.json())
 .then(json => {
     GBan = json;
-    console.log(JSON.stringify(GBan, null, 2));
+    console.debug(JSON.stringify(GBan, null, 2));
 })
 .catch(console.error);
